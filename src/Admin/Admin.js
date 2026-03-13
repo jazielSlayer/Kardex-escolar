cf4e8380843bdeb33a98c03c22a56d9c152220f9
Jaziel Vargas Choque <jazielarmandovargaschoque@gmail.com>
2026-03-13 18:18:09 -0400

    Corregimos detalles

--- src/Admin/Admin.js
+++ src/Admin/Admin.js
@@ -10,6 +10,10 @@ function Admin () {
 
                 <div>
                     <h1>Admin panel</h1>
+                    <h1>total de estudiantes</h1>
+                    <h1>Total de anotaciones</h1>
+                    <h1>Bajas traslados reingresos </h1>
+                    <h1>Reportes de cardexs individuales</h1>
                 </div>
             </div>
         </>
